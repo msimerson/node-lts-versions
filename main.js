1d563fad6a3ed0010ba75516e61115a025d7b19d
Matt Simerson <matt@tnpi.net>
2022-06-23 10:59:05 -0700

    fix: only the first named output was being set
    - bump @actions/core version to 1.9.0

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-// const core = require('@actions/core');
+const core = require('@actions/core');
+const useCore = false
+
 const l=require('./index')
 l.fetch().then(() => {
+  const lts    = l.json({lts: true})
   const active = l.json()
-  const min = JSON.stringify(JSON.parse(active)[0])
-  console.log(`::set-output name=active::${active} name=lts::${l.json({lts: true})} name=min::${min}`)
+  const min    = JSON.stringify(JSON.parse(active)[0])
+
+  if (useCore) {
+    core.setOutput('active', active)
+    core.setOutput('lts'   , lts)
+    core.setOutput('min'   , min)
+  }
+  else {
+    // console.log(`::set-output name=active::${active} name=lts::${lts} name=min::${min}`)
+    console.log(`::set-output name=active::${active}`)
+    console.log(`::set-output name=lts::${lts}`)
+    console.log(`::set-output name=min::${min}`)
+  }
 })
