d8282840242b6579430fe25c6fc1623c82fa61e9
Matt Simerson <matt@tnpi.net>
2023-08-02 11:29:27 -0700

    make dist

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const useCore = false
 
 const l=require('./index')
+
 l.fetch().then(() => {
   const lts    = l.json({lts: true})
   const active = l.json()
   const min    = JSON.stringify(JSON.parse(active)[0])
 
   if (useCore) {
-    // const core = require('@actions/core');
-    // core.setOutput('active', active)
-    // core.setOutput('lts'   , lts)
-    // core.setOutput('min'   , min)
+    const core = require('@actions/core');
+    core.setOutput('active', active)
+    core.setOutput('lts'   , lts)
+    core.setOutput('min'   , min)
   }
   else {
     // console.log(`::setOutput name=active::${active} name=lts::${lts} name=min::${min}`)
-    console.log(`::setOutput name=active::${active}`)
-    console.log(`::setOutput name=lts::${lts}`)
-    console.log(`::setOutput name=min::${min}`)
+    console.log(`::set-output name=active::${active}`)
+    console.log(`::set-output name=lts::${lts}`)
+    console.log(`::set-output name=min::${min}`)
   }
 })
